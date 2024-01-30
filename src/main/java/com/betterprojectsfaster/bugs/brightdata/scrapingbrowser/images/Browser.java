fed143db8d33248b4ea2d37266f9ef8bbd732073
Karsten Silz <karsten.silz@outlook.com>
2024-01-30 12:42:37 +0000

    General
    Added browser.

---
+++ src/main/java/com/betterprojectsfaster/bugs/brightdata/scrapingbrowser/images/Browser.java
@@ -0,0 +1,92 @@
+package com.betterprojectsfaster.bugs.brightdata.scrapingbrowser.images;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Browser {
+
+  @Value("#{new Boolean('${app.browser.remote.enabled:true}')}")
+  private boolean remoteBrowser;
+
+  @Value("#{new Boolean('${app.browser.blockPictures.enabled:true}')}")
+  private boolean blockPictures;
+
+  @Value("${app.browser.remote.url:''}")
+  private String remoteBrowserUrl;
+
+  private RemoteWebDriver driver;
+
+  @PostConstruct
+  void init() throws IOException {
+    log.info("Setting up Selenium...");
+
+    var options = new ChromeOptions();
+
+    if (blockPictures) {
+      Map<String, Object> prefs = new HashMap<>();
+      prefs.put("profile.managed_default_content_settings.images", 2);
+      options.setExperimentalOption("prefs", prefs);
+      log.info("Blocking pictures");
+    } else {
+      log.info("NOT blocking pictures");
+    }
+
+    if (remoteBrowser) {
+
+      if (StringUtils.isNotBlank(remoteBrowserUrl)) {
+        options.setCapability("browserName", "chrome");
+
+        var browserUrl = new URL(remoteBrowserUrl);
+
+        driver = new RemoteWebDriver(browserUrl, options);
+        log.info("Remote browser initialized.");
+      } else {
+        throw new InvalidArgumentException("Remote browser URL not set!");
+      }
+
+    } else {
+      System.setProperty("webdriver.http.factory", "jdk-http-client");
+      WebDriverManager.chromedriver().setup();
+      driver = new ChromeDriver(options);
+      log.info("Local browser initialized.");
+    }
+
+    log.info("Done setting up Selenium.");
+  }
+
+  public void goToPage() {
+
+    log.info("Opening page...");
+
+    try {
+      driver.get("https://www.cnn.com");
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    } catch (RuntimeException e) {
+      log.error("Error during warm-up", e);
+    } finally {
+      try {
+        log.info("Waiting for 60 seconds...");
+        Thread.sleep(60 * 1_000L);
+      } catch (InterruptedException e) {
+        //
+      }
+    }
+  }
+}
--- src/main/java/com/betterprojectsfaster/bugs/brightdata/scrapingbrowser/images/ImagesApplication.java
+++ src/main/java/com/betterprojectsfaster/bugs/brightdata/scrapingbrowser/images/ImagesApplication.java
@@ -1,13 +1,33 @@
 package com.betterprojectsfaster.bugs.brightdata.scrapingbrowser.images;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@Slf4j
+@RequiredArgsConstructor
 public class ImagesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImagesApplication.class, args);
-	}
+  @NotNull
+  @NonNull
+  private final Browser browser;
 
+  public static void main(String[] args) {
+    SpringApplication.run(ImagesApplication.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void run() {
+    log.info("run: START");
+    browser.goToPage();
+    log.info("run: STOP");
+    System.exit(0);
+  }
 }
